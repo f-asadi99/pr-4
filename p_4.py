3e5942ecda6bd454181f9b4eab18eb3e4de4b8a5
mahdieh <mahdieh.bh1rm@gmail.com>
2024-01-19 21:58:56 +0330

    create pr 4

---
+++ p_4.py
@@ -0,0 +1,19 @@
+# Edge detection of Image
+
+import cv2 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+img = cv2.imread('Ganesh.jpg') 
+image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+edges = cv2.Canny(image= image_rgb, threshold1=100, threshold2=700) 
+fig, axs = plt.subplots(1, 2, figsize=(7, 4)) 
+axs[0].imshow(image_rgb) 
+axs[0].set_title('Original Image') 
+axs[1].imshow(edges) 
+axs[1].set_title('Image edges') 
+for ax in axs: 
+	ax.set_xticks([]) 
+	ax.set_yticks([]) 
+plt.tight_layout() 
+plt.show()
